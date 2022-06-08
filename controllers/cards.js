5a2d2c9bb83956faa1df4555575a545ace36368c
AlexFDRK <alex.fdrk@gmail.com>
2022-06-08 23:13:25 +0300

    08-06.23:12

---
+++ controllers/cards.js
@@ -0,0 +1,70 @@
+const INCORRECT_DATA_CODE = 400;
+const NOT_FOUND_CODE = 404;
+const DEFAULT_ERROR_CODE = 500;
+
+const Card = require("../models/card");
+
+module.exports.getCards = (_req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((delCard) => res.send({ data: delCard }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(NOT_FOUND_CODE).send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link, likes, createdAt } = req.body;
+  const _id = req.user;
+
+  Card.create({ name, link, owner: _id, likes, createdAt })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(INCORRECT_DATA_CODE).send({ message: "Не заполнены обязательные поля" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(NOT_FOUND_CODE).send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(NOT_FOUND_CODE).send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
---
+++ controllers/users.js
@@ -0,0 +1,89 @@
+const INCORRECT_DATA_CODE = 400;
+const NOT_FOUND_CODE = 404;
+const DEFAULT_ERROR_CODE = 500;
+
+const User = require("../models/user");
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) =>
+      res.status(DEFAULT_ERROR_CODE).send({ message: err.message })
+    );
+};
+
+module.exports.findUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res
+          .status(NOT_FOUND_CODE)
+          .send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res
+          .status(INCORRECT_DATA_CODE)
+          .send({ message: "Не заполнены обязательные поля" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.patchMe = (req, res) => {
+  const { name, about } = req.body;
+  const _id = req.user;
+
+  User.findByIdAndUpdate(
+    _id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res
+          .status(INCORRECT_DATA_CODE)
+          .send({ message: "Не заполнены обязательные поля" });
+      } else if (err.name === "CastError") {
+        res
+          .status(NOT_FOUND_CODE)
+          .send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.patchAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const _id = req.user;
+
+  User.findByIdAndUpdate(_id, { avatar }, { new: true, runValidators: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res
+          .status(INCORRECT_DATA_CODE)
+          .send({ message: "Не заполнены обязательные поля" });
+      } else if (err.name === "CastError") {
+        res
+          .status(NOT_FOUND_CODE)
+          .send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+      }
+    });
+};
