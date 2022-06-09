e2faf31875e21a35a423d1cb2fb894d68a7d1c34
AlexFDRK <alex.fdrk@gmail.com>
2022-06-09 20:08:40 +0300

    09-06.20:08

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ module.exports.findUserById = (req, res) => {
     .catch((err) => {
       if (err.name === "CastError") {
         res
-          .status(NOT_FOUND_CODE)
+          .status(INCORRECT_DATA_CODE)
           .send({ message: "Запрашиваемая карточка не найдена" });
       } else {
         res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
