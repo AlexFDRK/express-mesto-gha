727df8f1a047fb668ea20f704aad5fd5a073c56d
AlexFDRK <alex.fdrk@gmail.com>
2022-06-23 22:08:51 +0300

    23-06.22:08

--- app.js
+++ app.js
@@ -29,16 +29,27 @@ app.post(
       password: Joi.string().required().min(2),
     }),
   }),
-  login,
+  login
 );
 
 app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
+      email: Joi.string()
+        .required()
+        .email({
+          minDomainSegments: 2,
+          tlds: { allow: ['com', 'net', 'ru'] },
+        }),
+      password: Joi.string().required().min(2),
       name: Joi.string().required().min(2).max(30),
       about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
+      avatar: Joi.string()
+        .required()
+        .pattern(
+          /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/
+        ),
     }),
   }),
   createUser,
