a3759284744779280aa94efaec95b44192631735
AlexFDRK <alex.fdrk@gmail.com>
2022-06-25 11:04:44 +0300

    25-06.11:04

--- routers/users.js
+++ routers/users.js
@@ -12,10 +12,25 @@ router.get('/', getUsers);
 router.get('/me', getMe);
 router.get(
   '/:id',
-  celebrate({ params: Joi.object().keys({ id: Joi.string().required().min(24).max(24) }) }),
+  celebrate({
+    params: Joi.object().keys({ id: Joi.string().required().min(24).max(24) }),
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
   findUserById,
 );
 router.patch('/me', patchMe);
-router.patch('/me/avatar', patchAvatar);
+router.patch(
+  '/me/avatar',
+  celebrate({
+    params: Joi.object().keys({ id: Joi.string().required().min(24).max(24) }),
+    avatar: Joi.string().pattern(
+      /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/
+    ),
+  }),
+  patchAvatar,
+);
 
 module.exports = router;
