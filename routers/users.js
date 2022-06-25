313780d1bcc67de47db7d3992b83292f5e6f71b7
AlexFDRK <alex.fdrk@gmail.com>
2022-06-25 10:57:28 +0300

    25-06.10:57

--- routers/users.js
+++ routers/users.js
@@ -15,7 +15,7 @@ router.get(
   celebrate({ params: Joi.object().keys({ id: Joi.string().required().min(24).max(24) }) }),
   findUserById,
 );
-router.patch('/patchMe', patchMe);
+router.patch('/me', patchMe);
 router.patch('/me/avatar', patchAvatar);
 
 module.exports = router;
